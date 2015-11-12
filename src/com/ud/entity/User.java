565392bcc5ddad2cb9dfc8540499a46e5093e5a7
tangtaiming <accountnum@sina.cn>
2015-11-12 13:01:43 +0800

    用户实体类

--- src/com/ud/entity/User.java
+++ src/com/ud/entity/User.java
@@ -1,5 +1,145 @@
 package com.ud.entity;
 
-public class User {
+import java.io.Serializable;
+
+/**
+ * 用户表
+ * 
+ * @author Administrator
+ * 
+ */
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -4353526750903108959L;
+	// ID
+	private int id;
+	// 姓名
+	private String name;
+	// 学号
+	private String studentID;
+	// 班级
+	private String classGrade;
+	// 专业
+	private String specialty;
+	// 学院
+	private String college;
+	// 所属学校id [外键]
+	private int schoolId;
+	// 类别
+	private int sort;
+	// 昵称
+	private String nickname;
+	// 邮箱
+	private String email;
+	// 账户名
+	private String accountName;
+	// 密码
+	private String password;
+	// 创建时间
+	private String createDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStudentID() {
+		return studentID;
+	}
+
+	public void setStudentID(String studentID) {
+		this.studentID = studentID;
+	}
+
+	public String getClassGrade() {
+		return classGrade;
+	}
+
+	public void setClassGrade(String classGrade) {
+		this.classGrade = classGrade;
+	}
+
+	public String getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+
+	public String getCollege() {
+		return college;
+	}
+
+	public void setCollege(String college) {
+		this.college = college;
+	}
+
+	public int getSchoolId() {
+		return schoolId;
+	}
+
+	public void setSchoolId(int schoolId) {
+		this.schoolId = schoolId;
+	}
+
+	public int getSort() {
+		return sort;
+	}
+
+	public void setSort(int sort) {
+		this.sort = sort;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
 
 }
